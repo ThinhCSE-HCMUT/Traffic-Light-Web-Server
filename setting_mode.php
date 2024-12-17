fb45e6e7580dbbb3ff6625647597f2102f627b58
Truong Thien <truongthien144@gmail.com>
2024-12-17 22:56:06 +0700

    fix for label

--- setting_mode.php
+++ setting_mode.php
@@ -12,11 +12,11 @@
                 <input class="form-control col-4 text-center text-danger" type="text" value="10s" aria-label="readonly input example" readonly>
             </div>
             <div class="input-group input-group-lg d-flex">
-                <label class="input-group-text text-uppercase text-white bg-success col-8 justify-content-center" for="Red Time">Timer Green Led</label>
+                <label class="input-group-text text-uppercase text-white bg-success col-8 justify-content-center" for="Green Time">Timer Green Led</label>
                 <input class="form-control col-4 text-center text-success" type="text" value="8s" aria-label="readonly input example" readonly>
             </div>
             <div class="input-group input-group-lg d-flex">
-                <label class="input-group-text text-uppercase text-white bg-warning col-8 justify-content-center" for="Red Time">Timer Yellow Led</label>
+                <label class="input-group-text text-uppercase text-white bg-warning col-8 justify-content-center" for="Yellow Time">Timer Yellow Led</label>
                 <input class="form-control col-4 text-center text-warning" type="text" value="2s" aria-label="readonly input example" readonly>
             </div>
             <a href="#" class="btn btn-rsv btn-lg btn-primary" role="button" title="Tap this to switch LED modifying">Switch LED Modifying</a>
